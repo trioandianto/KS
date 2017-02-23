0654833f83cb0768871ab7019ee0a871c6445de9
Trio Andianto <trio.andianto@basajans.com>
2017-02-23 14:33:46 +0700

    Add Action Fitur fitur screen

--- app/src/main/java/com/kliksembuh/ks/BookingActivity.java
+++ app/src/main/java/com/kliksembuh/ks/BookingActivity.java
@@ -1,14 +1,27 @@
 package com.kliksembuh.ks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class BookingActivity extends AppCompatActivity {
+    private TextView jadwal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+        jadwal =(TextView)findViewById(R.id.tvjadwal);
+
+        jadwal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), KonfirmasiJanjiActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
 
 
     }
--- app/src/main/java/com/kliksembuh/ks/KonfirmasiJanjiActivity.java
+++ app/src/main/java/com/kliksembuh/ks/KonfirmasiJanjiActivity.java
@@ -1,13 +1,45 @@
 package com.kliksembuh.ks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class KonfirmasiJanjiActivity extends AppCompatActivity {
+    private Button btnBuatJanji;
+    private ImageView editjadwal;
+    private ImageView editdokter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_konfirmasi_janji);
+        btnBuatJanji = (Button)findViewById(R.id.btnbuatjanji);
+        editdokter = (ImageView)findViewById(R.id.iveditdokter);
+        editjadwal = (ImageView)findViewById(R.id.iveditjadwal);
+        editdokter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), TestScroolView.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+        editjadwal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), BookingActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        btnBuatJanji.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), MyAppointmentActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
     }
 }
