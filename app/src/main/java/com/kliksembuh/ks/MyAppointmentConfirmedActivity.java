2b8d61b47de241591789811ac88d46c8119f6ccd
ucunurul <ucu.nurul.ulum@basajans.com>
2017-04-12 13:56:53 +0700

    Update MyAppointment Screen

--- app/src/main/java/com/kliksembuh/ks/MyAppointmentConfirmedActivity.java
+++ app/src/main/java/com/kliksembuh/ks/MyAppointmentConfirmedActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class MyAppointmentConfirmedActivity extends Fragment {
-
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
         View rootView = inflater.inflate(R.layout.activity_my_appointment_confirmed, container, false);
 
--- app/src/main/java/com/kliksembuh/ks/MyAppointmentSuccessActivity.java
+++ app/src/main/java/com/kliksembuh/ks/MyAppointmentSuccessActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class MyAppointmentSuccessActivity extends Fragment {
-
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
         View rootView = inflater.inflate(R.layout.activity_my_appointment_success, container, false);
 
