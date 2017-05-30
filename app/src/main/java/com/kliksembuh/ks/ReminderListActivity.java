fe801c14bbb781c9e2a0187444eeea2ecd35cfea
ucunurul <ucu.nurul.ulum@basajans.com>
2017-05-30 13:24:55 +0700

    Update ReminderListAdapter

--- app/src/main/java/com/kliksembuh/ks/ReminderListActivity.java
+++ app/src/main/java/com/kliksembuh/ks/ReminderListActivity.java
@@ -1,13 +1,180 @@
 package com.kliksembuh.ks;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.kliksembuh.ks.library.ReminderListAdapter;
+import com.kliksembuh.ks.models.Reminder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class ReminderListActivity extends AppCompatActivity {
+
+    private ListView lvReminder;
+    private ReminderListAdapter rAdapter;
+    private List<Reminder> mReminderList;
+    private ProgressDialog pDialog;
+    private Button btnAddReminder;
+    private String userID;
+    private String rmdrID;
+    private String rmdrTitle;
+    private String rmdrType;
+    private String rmdrDosage;
+    private String rdmrDays;
+    private String rmdrTimings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder_list);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        Bundle b = getIntent().getExtras();
+        if(b != null) {
+            userID = b.getString("userID");
+        }
+
+        lvReminder = (ListView) findViewById(R.id.listview_reminder);
+        btnAddReminder = (Button) findViewById(R.id.btn_addReminder);
+        btnAddReminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addRmdrIntent = new Intent(ReminderListActivity.this, SettingReminderDetailActivity.class);
+                Bundle b = new Bundle();
+                b.putString("userID", userID);
+                addRmdrIntent.putExtras(b);
+                startActivityForResult(addRmdrIntent, 1);
+
+            }
+        });
+        mReminderList = new ArrayList<>();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK){
+                userID = data.getStringExtra("userID");
+            }
+        }
+    }
+    public Drawable LoadImageFromWebOperations (String url){
+        try
+        {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable draw = Drawable.createFromStream(is, "src name");
+            return draw;
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+    public class ReminderListAsync extends AsyncTask<String, Void, String> {
+
+        public ReminderListAsync() {
+            super();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(ReminderListActivity.this);
+            pDialog.setProgress(R.drawable.pic_loading);
+            pDialog.setMessage("Mohon Menunggu...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            if ( netInfo != null && netInfo.isConnected()) {
+                try
+                {
+                    URL url = new URL("http://cloud.abyor.com:11080/kliksembuhapi/api/Institutions/SearchInstitutionFromAfterLogin?subDistrict=");
+                    HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
+                    urlc.setRequestProperty("Content-Type", "application/json");
+                    urlc.connect();
+                    int responseCode = urlc.getResponseCode();
+                    if (responseCode == HttpsURLConnection.HTTP_OK){
+                        BufferedReader in = new BufferedReader(
+                                new InputStreamReader(
+                                        urlc.getInputStream()));
+                        StringBuffer sb = new StringBuffer("");
+                        String line = "";
+                        while ((line = in.readLine()) != null){
+                            sb.append(line);
+                            break;
+                        }
+                        in.close();
+                        return sb.toString();
+                    }
+                    else {
+                        return "";
+                    }
+                } catch (MalformedURLException e1) {
+                    e1.printStackTrace();
+                    return "";
+                } catch (IOException ie) {
+                    ie.printStackTrace();
+                    return "";
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    return "";
+                }
+            }
+            else {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onCancelled(String s) {
+            super.onCancelled(s);
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
     }
 }
--- app/src/main/java/com/kliksembuh/ks/library/ReminderListAdapter.java
+++ app/src/main/java/com/kliksembuh/ks/library/ReminderListAdapter.java
@@ -1,9 +1,66 @@
 package com.kliksembuh.ks.library;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.kliksembuh.ks.R;
+import com.kliksembuh.ks.models.Reminder;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
 /**
  * Created by Ucu Nurul Ulum on 29/05/2017.
  */
 
-public class ReminderListAdapter {
+public class ReminderListAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<Reminder> mReminder;
 
+    public ReminderListAdapter(Context mContext, List<Reminder> mReminder) {
+        this.mContext = mContext;
+        this.mReminder = mReminder;
+    }
+
+    @Override
+    public int getCount() {
+        return mReminder.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mReminder.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View newRView = View.inflate(mContext, R.layout.list_reminder, null);
+        TextView tvRmdrTitle = (TextView) newRView.findViewById(R.id.tv_rmdrTitle);
+        TextView tvRmdrDosage = (TextView) newRView.findViewById(R.id.tv_rmdrDosage);
+        TextView tvRmdrDaysTiming =(TextView) newRView.findViewById(R.id.tv_rmdrDays);
+
+        String getTitle = mReminder.get(position).getRmdrTitle();
+        String getDosage = mReminder.get(position).getRmdrDosage();
+        String getDuration = mReminder.get(position).getRmdrDosage();
+        String getType = mReminder.get(position).getRmdrType();
+        String getDays = mReminder.get(position).getRmdrDays();
+        String getTimings = mReminder.get(position).getRmdrTimings();
+
+        tvRmdrTitle.setText(getTitle);
+        tvRmdrDosage.setText(getDosage);
+        tvRmdrDaysTiming.setText(getDays + " | " + getTimings);
+        newRView.setTag(mReminder.get(position).getRmdr_id());
+
+        return newRView;
+    }
 }
